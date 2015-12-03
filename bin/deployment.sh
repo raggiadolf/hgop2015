bfad43c6e728d76f00b8b86f0ff1ed314c3d10ae
ragnaradolf <ragnar.adolf@gmail.com>
2015-12-03 09:29:59 +0000

    Fixed deployment script to fit new folder structure

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -1,4 +1,4 @@
 sudo service docker start
 docker push raggiadolf/tictactoe
 echo "Docker image pushed to repo, sending setup script to test machine.\n"
-ssh vagrant@$1 'bash -s' < setuptest.sh
+ssh vagrant@$1 'bash -s' < bin/setuptest.sh
