b9dcc1849827968fae9d05529797e1b267853371
ragnaradolf <ragnar.adolf@gmail.com>
2015-12-09 10:40:04 +0000

    Added first acceptance test which creates game and then retrieves it using gameHistory

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -9,4 +9,42 @@ describe('TEST ENV GET /api/gameHistory', function() {
   it('should have ACCEPTANCE_URL environment variable exported.', function() {
     acceptanceUrl.should.be.ok;
   });
+
+  it('should execute same test using old style', function(done) {
+    var command = {
+      eventID: "1234",
+      gameID: "999",
+      command: "CreateGame",
+      userName: "Raggi",
+      gameName: "TheFirstGame",
+      timeStamp: "2015.12.09T09:54:17"
+    };
+
+    var req = request(acceptanceUrl);
+    req
+      .post('/api/createGame')
+      .type('json')
+      .send(command)
+      .end(function(err, res) {
+        if (err) return done(err);
+        request(acceptanceUrl)
+          .get('/api/gameHistory/999')
+          .expect(200)
+          .expect('Content-Type', 'application/json')
+          .end(function(err, res) {
+            if(err) return done(err);
+            res.body.should.be.instanceof(Array);
+            should(res.body).eql(
+              [{
+                "eventID": "1234",
+                "gameID": "999",
+                "event": "GameCreated",
+                "userName": "Raggi",
+                "gameName": "TheFirstGame",
+                "timeStamp": "2015.12.09T09:54:17"
+              }]);
+            done();
+          });
+      });
+  });
 });
