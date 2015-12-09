7ee730e7a2e7de73c973efd5f2b9964a1715052a
ragnaradolf <ragnar.adolf@gmail.com>
2015-12-09 13:50:19 +0000

    Added fluid acceptance test

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -49,10 +49,68 @@ describe('TEST ENV GET /api/gameHistory', function() {
   });
 
   it('should execute fluid API test', function(done) {
-    /*
-      given(user("YourUser").createGame("TheFirstGame"))
-      .expect("GameCreated").withName("TheFirstGame").isOk(done);
-      */
-    done();
+    var event = {
+      eventID: "1234",
+      gameID: "666",
+      command: "CreateGame",
+      userName: "Raggi",
+      gameName: "TheFirstGame",
+      timeStamp: "2015.12.09T09:54:17"
+    };
+
+    var destination = '/api/createGame';
+
+    var expectation = [{
+      "eventID": "1234",
+      "gameID": "666",
+      "event": "GameCreated",
+      "userName": "Raggi",
+      "gameName": "TheFirstGame",
+      "timeStamp": "2015.12.09T09:54:17"
+    }];
+
+    given(event).sendTo(destination).expect(expectation).when(done);
   });
 });
+
+function given(event) {
+  var command = {
+    name: event,
+    destination: undefined
+  };
+
+  var expectations = [];
+
+  var givenApi = {
+    sendTo: function(dest) {
+      command.destination = dest;
+      return givenApi;
+    },
+    expect: function(eventName) {
+      expectations.push(eventName);
+      return givenApi;
+    },
+    //and: givenApi.expect,
+    when: function(done) {
+      var req = request(acceptanceUrl);
+      req
+        .post(command.destination)
+        .type('json')
+        .send(command.event)
+        .end(function(err, res) {
+          if(err) return done(err);
+          request(acceptanceUrl)
+            .get('/api/gameHistory/' + command.event.gameID)
+            .expect(200)
+            .expect('Content-Type', 'application/json')
+            .end(function(err, res) {
+              if(err) return done(err);
+              res.body.should.be.instanceof(Array);
+              should(res.body).eql(expectations);
+            });
+        });
+      done();
+    }
+  };
+  return givenApi;
+}
