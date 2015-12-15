d6b4a040716a3839d81451a766175f49b6759f20
ragnaradolf <ragnar.adolf@gmail.com>
2015-12-15 15:17:34 +0000

    Fixing scripts for day 10

--- bin/setuptest.sh
+++ bin/setuptest.sh
@@ -6,8 +6,8 @@ docker rm -f tictactoe$2
 
 echo Getting latest image from docker hub
 echo
-docker pull raggiadolf/tictactoe$2
+docker pull raggiadolf/tictactoe:$2
 
 echo Running the docker container
 echo
-docker run -p 8080:$1 -d --name tictactoe$2 -e "NODE_ENV=production" raggiadolf/tictactoe$2
+docker run -p $1:8080 -d --name tictactoe$2 -e "NODE_ENV=production" raggiadolf/tictactoe:$2
