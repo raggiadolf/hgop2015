b79e1e778dd917d89ebe4c80a48f72005cfa8a30
ragnaradolf <ragnar.adolf@gmail.com>
2015-12-15 21:17:15 +0000

    Fixed error when displaying joined player username

--- client/app/joinGame/joinGame.controller.js
+++ client/app/joinGame/joinGame.controller.js
@@ -14,12 +14,11 @@ angular.module('tictactoeApp')
     thenHandleEvents($http.get('/api/gameHistory/' + $state.params.gameID));
     
     $scope.joinGame = function() {
-      var user = {'userName': $scope.userName, side: 'O'};
       var joinPostPromise = $http.post('/api/joinGame/', {
         'eventID': guid(),
         'gameID': $scope.gameState.gameID,
         'command': 'JoinGame',
-        'user': user,
+        'userName': $scope.userName,
         'timeStamp': '2015.12.14T22:04:03'
       });
       thenHandleEvents(joinPostPromise);
