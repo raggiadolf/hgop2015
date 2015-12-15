48510348e95a625655f49bffd270d9832763043f
ragnaradolf <ragnar.adolf@gmail.com>
2015-12-15 11:34:18 +0000

    Fixed typo in guid provider, semi-working version deployed, can start a game, but can't make a move

--- client/app/app.js
+++ client/app/app.js
@@ -16,7 +16,7 @@ angular.module('tictactoeApp', [
     function s4() {
       return Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
-        .subString(1);
+        .substring(1);
     }
     function calculate() {
       return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
